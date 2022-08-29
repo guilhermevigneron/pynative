9d47b267143d2870a8cb4ad0dd46c8c57e55ad86
Guilherme <guilhermevigneron@gmail.com>
2022-08-28 20:05:31 -0500

    Update test cases

--- basic.py
+++ basic.py
@@ -13,42 +13,50 @@ class Exercises:
 
     def print_sum(self, number: int) -> str:
         """2"""
+        result_list = []
         print("Printing current and previous number sum in a range(", number, ")")
         for x in range(number):
             print("Current Number ", x, "Previous Number  ",
                   x-1, "  Sum:  ", x + (x-1))
+            result_list.append([x, x-1, x+(x-1)])
+        return result_list
 
-    def print_even_char(self, string: str) -> str:
+    def print_even_char(self, string: str) -> list:
         """3"""
+        result_string = ""
         print("Orginal String is  ", str)
         print("Printing only even index chars")
         for char in string:
             if (string.find(char) % 2) == 0:
-                print(char)
+                result_string += char
+
+        return result_string
 
     def remove_char(self, str: str, index: int) -> str:
         """4"""
-        return print(str[index:])
+        return str[index:]
 
     def first_last_same(self, numbers_x: list) -> bool:
         """5"""
         if numbers_x[0] == numbers_x[-1]:
-            return print(True)
+            return True
         else:
-            return print(False)
+            return False
 
-    def divisible_by_5(self, number_list: list) -> bool:
+    def divisible_by_5(self, number_list: list) -> list:
         """6"""
+        result_list = []
         print("Given list is ", number_list)
         print("Divisible by 5")
         for number in number_list:
             if number % 5 == 0:
-                print(number)
+                result_list.append(number)
 
+        return result_list
 
-#execute.multiply_or_sum(40, 30)
+# execute.multiply_or_sum(40, 30)
 # execute.print_sum(10)
 # execute.print_even_char("pynative")
-#execute.remove_char("pynative", 2)
-#execute.first_last_same([10, 20, 32, 40, 11])
-#execute.divisible_by_5([10, 20, 32, 40, 11])
+# execute.remove_char("pynative", 2)
+# execute.first_last_same([10, 20, 32, 40, 11])
+# execute.divisible_by_5([10, 20, 32, 40, 11])
--- test_basic.py
+++ test_basic.py
@@ -8,6 +8,23 @@ class Test_basic(unittest.TestCase):
     def test_multiply_or_sum(self):
         self.assertEqual(exercise.multiply_or_sum(3, 4), 12)
 
+    def test_print_sum(self):
+        self.assertEqual(exercise.print_sum(
+            3), [[0, -1, -1], [1, 0, 1], [2, 1, 3]])
+
+    def test_print_even_char(self):
+        self.assertEqual(exercise.print_even_char("pynative"), "pntv")
+
+    def test_remove_char(self):
+        self.assertEqual(exercise.remove_char("pynative", 2), "native")
+
+    def test_first_last_name(self):
+        self.assertEqual(exercise.first_last_same([10, 20, 30, 40, 10]), True)
+
+    def test_divisible_by_5(self):
+        self.assertEqual(exercise.divisible_by_5(
+            [10, 20, 33, 46, 55]), [10, 20, 55])
+
 
 if __name__ == '__main__':
     unittest.main()
